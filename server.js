8cc8913da408249640a466c9b8fcef6d38d1b77f
noufanelachola <noufansadiqelachola@gmail.com>
2024-07-14 17:58:16 +0530

    before

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 
+const sendMail = require("./controllers/sendMail")
+
 const app = express();
 app.use(cors());
 
@@ -10,6 +12,8 @@ app.use(express.urlencoded({ extended: false }));
 app.get("/",(req,res)=>{
     res.json("Hello");
 });
+
+app.get("/sendmail",(req,res)=>{sendMail.sendMail(req,res)});
 
 app.listen(3000,() => {
     console.log(`App is running on port 3000`);
